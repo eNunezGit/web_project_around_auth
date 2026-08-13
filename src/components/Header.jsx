5b3014e04aec3db319540ab751132ef334956b9f
KikeNunez <enrique.nunez.atiaga@gmail.com>
2026-08-13 10:56:04 -0600

    Refactor Header component: update layout and button functionality based on authentication state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,26 @@
-import headerLogo from '../images/header-logo.svg'
+import { useContext } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { LoginContext } from '../contexts/LoginContext.js';
+
+import headerLogo from '../images/header-logo.svg';
 
 function Header() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const route = location.pathname;
+
+  const { isLoggedIn } = useContext(LoginContext);
+
+  const handleButtonClick = () => {
+    if (!isLoggedIn) {
+      route === "/signin"
+      ? navigate("/signup")
+      : navigate("/signin")
+    } else {
+      // elimina el jwt :P
+    }
+  };
+
   return (
     <header className="header page__section">
       <div className="header__logo-container">
@@ -9,8 +29,21 @@ function Header() {
           src={headerLogo}
           alt="Alrededor de E.U." />
       </div>
+      {!isLoggedIn
+      ? (<div className="header__container">
+          <button className="header__button" onClick={handleButtonClick}>
+            <span className="header__text">{route === "/signin" ? "Regístrate" : "Iniciar sesión"}</span>
+          </button>
+        </div>)
+      : (<div className="header__container">
+          <p className="header__text">example@gmail.com</p>
+          <button className="header__button" onClick={handleButtonClick}>
+            <span className="header__text_gray">Cerrar sesión</span>
+          </button>
+        </div>)
+      }
     </header>
   );
 }
 
-export default Header
+export default Header;
